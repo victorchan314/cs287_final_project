6ab9ef91d8c934e8376fa2f4dd4befcbae846266
Victor Chan <victorchan88888888@gmail.com>
2019-12-11 12:36:04 -0800

    Dynamics sampler

--- rlkit/samplers/dynamics.py
+++ rlkit/samplers/dynamics.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from rlkit.dynamics.mlp_model import MlpModel
 from rlkit.samplers.util import rollout
 from rlkit.torch.sac.policies import MakeDeterministic
 
@@ -9,11 +10,17 @@ class DynamicsSampler(object):
     A sampler that uses samples to learn a dynamics model
     before using the dynamics model to generate samples.
     """
-    def __init__(self, env, policy, max_path_length):
+    def __init__(self, env, policy, max_path_length, num_train_itr=50, num_train_steps_per_itr=50, tandem_train=True, **kwargs):
         self.env = env
         self.policy = policy
 
         self.max_path_length = max_path_length
+        self.num_train_itr = num_train_itr
+        self.num_train_steps_per_itr = num_train_steps_per_itr
+        self.tandem_train = tandem_train
+
+        self.model = MlpModel(**kwargs)
+        self.training = True
 
     def start_worker(self):
         pass
@@ -22,19 +29,19 @@ class DynamicsSampler(object):
         pass
 
     def obtain_samples(self, deterministic=False, max_samples=np.inf, max_trajs=np.inf, accum_context=True, resample=1):
-        """
-        Obtains samples in the environment until either we reach either max_samples transitions or
-        num_traj trajectories.
-        The resample argument specifies how often (in trajectories) the agent will resample it's context.
-        """
         assert max_samples < np.inf or max_trajs < np.inf, "either max_samples or max_trajs must be finite"
         policy = MakeDeterministic(self.policy) if deterministic else self.policy
         paths = []
         n_steps_total = 0
         n_trajs = 0
         while n_steps_total < max_samples and n_trajs < max_trajs:
-            path = rollout(
-                self.env, policy, max_path_length=self.max_path_length, accum_context=accum_context)
+            if self.training:
+                for i in range(self.num_train_steps_per_itr):
+                    path = rollout(self.env, policy, max_path_length=self.max_path_length, accum_context=accum_context)
+                    self.model.train(paths)
+            else:
+                path = rollout(self.model, policy, max_path_length=self.max_path_length, accum_context=accum_context)
+
             # save the latent context that generated this trajectory
             path['context'] = policy.z.detach().cpu().numpy()
             paths.append(path)
@@ -43,5 +50,9 @@ class DynamicsSampler(object):
             # don't we also want the option to resample z ever transition?
             if n_trajs % resample == 0:
                 policy.sample_z()
+
+            if n_steps_total > self.num_train_itr:
+                self.training = False
+
         return paths, n_steps_total
 
